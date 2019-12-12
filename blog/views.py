6333497048400d879d16cf80129456c1f0d86100
Ike <ikescode@gmail.com>
2019-12-11 19:57:53 -0800

    use template inheritance

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
+posts = [
+    {
+        'author': 'Ike',
+        'title': 'Cool chord progression',
+        'content': 'F Am C G',
+        'date_posted': 'December 10th, 2019'
+    },
+    {
+        'author': 'Bob',
+        'title': 'Awesome melody',
+        'content': 'C E G B G E C in eighth notes at 145 bpm!',
+        'date_posted': 'December 11th, 2019'
+    },
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts,
+        'title': 'Home'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return render(request, 'blog/about.html')
+    context = {
+        'title': 'About'
+    }
+    return render(request, 'blog/about.html', context)
