b065934fd5fab3fb11f1188e0e6691e5b55a49c9
Ike <ikescode@gmail.com>
2019-12-13 15:10:24 -0800

    remove dummy data, format date on posts

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Ike',
-        'title': 'Cool chord progression',
-        'content': 'F Am C G',
-        'date_posted': 'December 10th, 2019'
-    },
-    {
-        'author': 'Bob',
-        'title': 'Awesome melody',
-        'content': 'C E G B G E C in eighth notes at 145 bpm!',
-        'date_posted': 'December 11th, 2019'
-    },
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all(),
